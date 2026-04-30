f9cdeb76cad830330a15b31168e4705aa0271d4a
caps2230 <cristhianp.soft@gmail.com>
2026-04-30 15:54:05 -0500

    feat: Implementar animaciones de verificación y escaneo táctiles del ADN

---
+++ lib/screens/terminal_scren.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainTerminalScreen extends StatelessWidget {
+  const MainTerminalScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
